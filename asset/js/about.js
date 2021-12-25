62187c2bb95d8fa1ef35165d4ebf5cc90f0311ed
RedInstealth <redinstealth@gmail.com>
2021-12-25 09:12:22 -0600

    Files

---
+++ asset/js/about.js
@@ -0,0 +1,2 @@
+$(function(){$(".logo").click(function(){$(".svglogo").toggleClass("open");$(".left").toggleClass("sliderleft");$(".right").toggleClass("sliderright");$(".svglogo").fadeOut(1000);setTimeout(function()
+{$("html").css("overflow-y","scroll");},3500);});});
